2c66099fe984288ff5a40a4f092f6c8a71473d86
alancorleto <corletoalan@gmail.com>
2026-04-15 03:46:10 -0300

    Add support for a test SQLite database when running locally

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+import os
+
 app = FastAPI()
 
 @app.get("/")
 def main():
     return {"message": "Hello World"}
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    secret_name: str
+    age: int | None = Field(default=None, index=True)
+
+
+# The database URL is automatically injected as an environment variable
+engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///local_test_database.db"))
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/heroes/")
+def create_hero(hero: Hero):
+    with Session(engine) as session:
+        session.add(hero)
+        session.commit()
+        session.refresh(hero)
+        return hero
+
+
+@app.get("/heroes/")
+def read_heroes():
+    with Session(engine) as session:
+        heroes = session.exec(select(Hero)).all()
+        return heroes
+
