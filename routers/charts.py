657ff624ea864f2e4f363b4ae719bb98b449f42c
alancorleto <corletoalan@gmail.com>
2026-04-20 19:49:43 -0300

    Implement update chart endpoint

--- routers/charts.py
+++ routers/charts.py
@@ -37,10 +37,18 @@ async def create_chart(chart: ChartCreate, session: SessionDep):
     return db_chart
 
 
-@router.put("/{chart_id}")
-async def update_chart(chart_id: int):
+@router.patch("/{chart_id}")
+async def update_chart(chart_id: uuid.UUID, chart: ChartUpdate, session: SessionDep):
     """Update a chart"""
-    return {"chart_id": chart_id, "message": "Chart updated"}
+    db_chart = session.get(Chart, chart_id)
+    if not db_chart:
+        raise HTTPException(status_code=404, detail="Chart not found")
+    chart_data = chart.model_dump(exclude_unset=True)
+    db_chart.sqlmodel_update(chart_data)
+    session.add(db_chart)
+    session.commit()
+    session.refresh(db_chart)
+    return db_chart
 
 
 @router.delete("/{chart_id}")
