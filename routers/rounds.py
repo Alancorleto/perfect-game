10b82ae7218f5b444fb3599437ad9c6884983d45
alancorleto <corletoalan@gmail.com>
2026-04-21 15:44:00 -0300

    Correct round endpoint functions signature

--- routers/rounds.py
+++ routers/rounds.py
@@ -1,4 +1,7 @@
+import uuid
 from fastapi import APIRouter
+from models.round import Round, RoundCreate, RoundUpdate, RoundPublic
+from database import SessionDep
 
 router = APIRouter(
     prefix="/rounds",
@@ -7,30 +10,30 @@ router = APIRouter(
 
 
 @router.get("/")
-async def list_rounds():
+async def list_rounds(session: SessionDep):
     """List all rounds"""
     return {"message": "List of rounds"}
 
 
 @router.get("/{round_id}")
-async def get_round(round_id: int):
+async def get_round(round_id: uuid.UUID, session: SessionDep):
     """Get a specific round"""
     return {"round_id": round_id}
 
 
 @router.post("/")
-async def create_round():
+async def create_round(round: RoundCreate, session: SessionDep):
     """Create a new round"""
     return {"message": "Round created"}
 
 
 @router.put("/{round_id}")
-async def update_round(round_id: int):
+async def update_round(round_id: uuid.UUID, round: RoundUpdate, session: SessionDep):
     """Update a round"""
     return {"round_id": round_id, "message": "Round updated"}
 
 
 @router.delete("/{round_id}")
-async def delete_round(round_id: int):
+async def delete_round(round_id: uuid.UUID, session: SessionDep):
     """Delete a round"""
     return {"round_id": round_id, "message": "Round deleted"}
